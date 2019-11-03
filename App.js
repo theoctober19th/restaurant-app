3765409fc341c1f3d4df5d4bd1ecf44292b97a23
Bikalpa Dhakal <theoctober19th@gmail.com>
2019-11-03 16:14:26 +0545

    created empty project

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet
+} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+    </>
+  );
+};
+
+export default App;
